ba5fdaa161fb6bfd36295d100b3495bb92c4c495
Mateusz <mat646@gmail.com>
2019-05-26 17:58:45 +0200

    Implemented preproc, only nan

--- src/preprocessing/preproc.py
+++ src/preprocessing/preproc.py
@@ -2,6 +2,14 @@ import pandas as pd
 import numpy as np
 
 
+def map_fl(value: str):
+    trimmed_str = value.replace(',', '.').replace('"', '').replace(' ', '')
+    if not trimmed_str or 'innewyniki' in trimmed_str:
+        return np.nan
+    else:
+        return float(trimmed_str)
+
+
 def map_cywilny(value: str):
     value = value.lower()
     if 'wolny' in value:
@@ -14,10 +22,80 @@ def map_cywilny(value: str):
         return np.nan
 
 
+def map_praca(value: str):
+    value = value.lower()
+    if 'renta' in value:
+        return 0.0
+    elif 'emeryt' in value:
+        return 0.0
+    elif 'aktywny' in value:
+        return 1.0
+    else:
+        return np.nan
+
+
+def map_tryb_pracy(value: str):
+    value = value.lower()
+    if 'siedzaca' in value:
+        return 0.0
+    elif 'ruch/siedzaca' in value:
+        return 1.0
+    elif 'ruch' in value:
+        return 2.0
+    else:
+        return np.nan
+
+
+def map_akt_fiz(value: str):
+    value = value.lower()
+    if 'nigdy' in value:
+        return 0.0
+    elif 'sporadycznie' in value:
+        return 1.0
+    elif 'regularnie' in value:
+        return 2.0
+    else:
+        return np.nan
+
+
+def map_lie_min(value: str):
+    if '<40' in value:
+        return 0.0
+    elif '>40' in value:
+        return 1.0
+    else:
+        return np.nan
+
+
+def map_sol(value: str):
+    value = value.lower()
+    if 'nigdy' in value:
+        return 0.0
+    elif 'rzadko' in value:
+        return 1.0
+    elif 'czasem' in value:
+        return 2.0
+    elif 'często' in value:
+        return 3.0
+    elif 'zawsze' in value:
+        return 4.0
+    else:
+        return np.nan
+
+
 def read_dfs():
     df_phen = pd.read_csv('ucn2aghphen.txt', sep='  |\t', header=0, engine='python', index_col=False)
-    df_phen = df_phen.drop(['"elem"', '"numer"', '"X"'], axis=1)
+    df_phen = df_phen.drop(['"elem"', '"numer"', '"X"',
+                            '"wyksztalcenie"', '"lata"', '"zawod"', '"przychod"', '"dyscyplina"'], axis=1)
+
     df_phen['"cywilny"'] = df_phen['"cywilny"'].map(map_cywilny)
+    df_phen['"praca"'] = df_phen['"praca"'].map(map_praca)
+    df_phen['"sol"'] = df_phen['"sol"'].map(map_sol)
+    df_phen['"ile_min"'] = df_phen['"ile_min"'].map(map_lie_min)
+    df_phen['"akt_fiz"'] = df_phen['"akt_fiz"'].map(map_akt_fiz)
+    df_phen['"tryb_pracy"'] = df_phen['"tryb_pracy"'].map(map_sol)
+    df_phen.loc[:, df_phen.dtypes == object] = df_phen.loc[:, df_phen.dtypes == object].applymap(map_fl)
+
     df_xp = pd.read_csv('ucn2aghxp.txt', sep='  |\t', engine='python').transpose()
 
     return df_phen, df_xp
