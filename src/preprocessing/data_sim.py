4fa0614f70ad9ca6ed783dbfd1ddb25bbf6227aa
Mateusz <mat646@gmail.com>
2019-09-18 22:43:26 +0200

    Implemented data_sim

--- src/preprocessing/data_sim.py
+++ src/preprocessing/data_sim.py
@@ -1,33 +1,37 @@
 import numpy as np
-from numpy.random import multivariate_normal, rand, uniform, random
-from numpy import tril
+from numpy.random import multivariate_normal, uniform, random
 
 
 def data_sim():
+    no_features = 50
+    no_samples = 300
 
-    p = 60
-    N = 300
+    mean = uniform(low=-10.0, high=10.0, size=(no_features,))
+    variance = uniform(low=-1.0, high=1.0, size=(no_features, no_features,))
 
-    mean = uniform(low=-10.0, high=10.0, size=(p,))
-    variance = uniform(low=-1.0, high=1.0, size=(p, p,))
+    takes = multivariate_normal(mean, variance, no_samples)
 
-    takes = multivariate_normal(mean, variance, N)
+    size = [10, 10, 10]  # remaining p - sum(size) is reference
 
-    size_A1 = 10
-    size_A2 = 10
-    size_A3 = 10
-    size_A4 = 20
+    X_idx = [np.sum(size[:i]) for i in range(len(size) + 1)]
+    X = [takes[:, int(l):int(r)] for l, r in zip(X_idx, X_idx[1:])]
 
-    alfa_A1 = uniform(low=-1.0, high=1.0, size=(size_A1,))
-    delta_A1 = random(1)[0]
+    alpha = [uniform(low=-1.0, high=1.0, size=(s,)) for s in size]
+    delta = random(len(size)) * 20
 
-    alfa_A2 = uniform(low=-1.0, high=1.0, size=(size_A2,))
-    delta_A2 = random(1)[0]
+    val = [np.dot(x, a) for x, a in zip(X, alpha)]
+    label = [v > d for v, d in zip(val, delta)]
 
-    vals = np.dot(takes[:, :10], alfa_A1)
+    arr = np.empty(300, int)
 
-    # TODO now filter that vals above delta_A2 and we have labels
+    for i in range(300):
+        if label[0][i]:
+            arr[i] = 1
+        elif label[1][i]:
+            arr[i] = 2
+        elif label[2][i]:
+            arr[i] = 3
+        else:
+            arr[i] = 4
 
-    # TODO Classification
-
-    # TODO Repeat 10k
+    return takes, arr
